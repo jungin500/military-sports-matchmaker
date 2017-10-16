c8317649539c2e3c354c67f5b63e4e79400a32be
bumsook <bumsook@princeton.edu>
2017-10-17 00:09:21 +0900

    로그인, 회원가입 기본 형태

--- client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/MainActivity.java
+++ client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/MainActivity.java
@@ -1,13 +1,32 @@
 package kr.oss.sportsmatchmaker.militarysportsmatchmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+// MainActivity is a splash screen where we decide whether to go or not.
 public class MainActivity extends AppCompatActivity {
+
+    //TODO: fix this using SharedPref
+    boolean isLoggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: update using SharedPref
+
+        //TODO: go to core activity
+        if (isLoggedIn) {
+
+        }
+        // goto sign in screen.
+        else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        finish();
     }
 }
