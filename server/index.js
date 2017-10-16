feb1367c13c3922e914d1155bfe337a59b68b901
안정인 <ji5489@gmail.com>
2017-10-17 00:09:50 +0900

    서버 수정사항 반영: 사용자 관리 Model 및 Schema 구현 완료
    Signed-off-by: 안정인 <ji5489@gmail.com>

--- server/index.js
+++ server/index.js
@@ -12,13 +12,19 @@
 
 var http = require('http'),
     express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('session'),
+    session = require('express-session'),
     expressErrorHandler = require('express-error-handler'),
+    bodyParser = require('body-parser'),
     //static = require('serve-static'),
     //path = require('path'),
     
+    crypto = require('crypto'),
     mongoose = require('mongoose');
+
+// express 이용 HTTP 서버 설정
+var app = express();
+app.set('port', process.env.PORT || 14402);
+app.set('mongoose-reconnect-max', 5);
 
 // MongoOSE 이용 DB 초기화
 var database;
@@ -34,17 +40,101 @@ var Model = {
 };
 
 function connectDB() {
-    var userDBUrl = 'mongodb://localhost:27017/users';
-    var matchingDBUrl = 'mongodb://localhost:27017/matching';
+    var databaseUrl = 'mongodb://localhost:27017/matching';
 
-    var matching
+    mongoose.Promise = global.Promise;
+    mongoose.connect(databaseUrl, { useMongoClient: true });
+    database = mongoose.connection;
+
+    database.on('error', console.error.bind(console, '[심각] MongoDB 연결 오류'));
+    database.on('open', function() {
+        console.log('[정보] MongoDB 연결 성공');
+        createSchema();
+
+        database.on('disconnected', function() {
+            var tries;
+            if(!(tries = app.get('mongoose-reconnect-try')))
+                app.set('mongodb-reconnect-try', 1);
+            else if (tries >= app.get('mongoose-reconnect-max')) {
+                console.log('[심각] MongoDB 연결 불가능. 서버를 종료합니다.ㄴ');
+                return;
+            }
+            app.set('mongodb-reconnect-try', tries + 1);
+
+            console.log('[심각] MongoDB 연결 끊김. 5초 뒤 재연결 시도합니다.');
+            setTimeout(connectDB, 5000);
+        });
+    });
 }
 
-// express 이용 HTTP 서버 설정
-var app = express();
-app.set('port', process.env.PORT || 14402);
+function createSchema() {
 
+    // 사용자 스키마
+    Schema.user = mongoose.Schema({
+        username: { type: String, required: true, unique: true, 'default': ' ' },
+        hashed_password: { type: String, required: true, unique: false, 'default': ' ' },
+        salt: { type: String, required: true },
+        name: { type: String, index: 'hashed', 'default': ' ' },
+        budae: { type: String, index: 'hashed', 'default': ' ' },
+        milId: { type: String, required: true, unique: true, 'default': ' ' },
+        favoriteEvent: { type: Object, required: false, 'default': {} },
+        description: { type: String, required: true, unique: false, 'default': ' ' },
+        created_at: { type: Date, index: { unique: false }, 'default': Date.now },
+        updated_at: { type: Date, index: { unique: false }, 'default': Date.now }
+    });
+
+    Schema.user.virtual('password').set(function(plaintext) {
+        this._plaintext = plaintext;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptSHA1(plaintext);
+    }).get(function() {
+        return this._plaintext;
+    });
+
+    Schema.user.method('encryptSHA1', function(plaintext, salt) {
+        return crypto.createHmac('sha1', salt || this.salt).update(plaintext).digest('hex');
+    });
+
+    Schema.user.method('auth', function(plaintext, salt, hashed_password) {
+        return this.encryptSHA1(plaintext, salt || null) == hashed_password;
+    });
+
+    Schema.user.static('authenticate', function(userInfo, callback) {
+        this.find({ username: userInfo.username }, function(err, result) {
+            if(err) throw err;
+
+            var resultDoc = result._doc;
+            if(resultDoc) {
+                var user = new Model.user({ username: userInfo.username });
+                if(user.auth(userInfo.password, resultDoc[0].salt, resultDoc[0].hashed_password))
+                    callback(true);
+                else
+                    callback(false);
+                return;
+            } else
+                callback(null);
+        });
+    });
+
+    // 경기 매칭 스키마
+    Schema.matching = mongoose.Schema({
+        activityType: { type: String, required: true, unique: false, 'default': ' ' },
+        participants: { type: Object, required: true, unique: false, 'default': {} },
+        start_at: { type: String, required: true, unique: false, 'default': ' ' }
+    });
+}
+
+function createUser(userInfo, callback) {
+    var User = new Model.user(userInfo);
+    User.save(function(err){
+        if(err) callback(err);
+        else callback(null);
+    });
+}
+
+// HTTP 서버 구동
 http.createServer(app).listen(app.get('port'), function() {
+    connectDB();
     console.log('[정보] 서버 시작됨. %d에서 listen 중', app.get('port'));
 });
 
@@ -56,6 +146,7 @@ router.route('/process/registerUser').post(function(req, res) {
     var userInfo = {
         username: req.body.username,
         password: req.body.password,
+        name: req.body.name,
         budae: req.body.budae,
         milId: req.body.milId,
         favoriteEvent : req.body.favoriteEvent,
@@ -71,8 +162,58 @@ router.route('/process/registerUser').post(function(req, res) {
         }
 
     // 가져온 정보를 MongoOSE 이용하여 DB에 저장
-
+    createUser(userInfo, function(err) {
+        if(err) throw err;
+        console.log('[정보] 회원가입 완료: ID [%s]', userInfo.username);
+    });
 });
 
+router.route('/process/loginUser').post(function(req, res) {
+    var userInfo = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    Model.user.authenticate(userInfo, function(result) {
+        if(result) {
+            console.log('[알림] 로그인 성공!');
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+            res.end('<h1>로그인 성공</h1>');
+        } else if (result == null)
+            console.log('[알림] 해당 사용자가 없습니다.');
+        else if (result == false)
+            console.log('[알림] 사용자의 비밀번호가 틀립니다.');
+    });
+});
+
+router.route('/process/getMatchList').get(function(req, res) {
+    // 현재 진행중인 Match 목록
+});
+
+router.route('/process/addMatch').post(function(req, res) {
+    // 새로 추가된 Match
+    // 무결성 검증 필요
+});
+
+router.route('/process/heartbeat').get(function(req, res) {
+    // Heartbeat
+    res.writeHead(200, {'Content-Type':'text/html;charset=utf8'});
+    res.end('Success');
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(router);
+
+app.use(session({
+    secret: '123a',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(expressErrorHandler.httpError(404));
+app.use(expressErrorHandler({
+    static: {
+        '404': './include/404.html'
+    }
+}));
