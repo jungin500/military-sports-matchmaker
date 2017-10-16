f81af9a23516b7b1bf79fb0500a1bd7de7a1209a
안정인 <ji5489@gmail.com>
2017-10-16 22:01:43 +0900

    수정중인 index.js
    Signed-off-by: 안정인 <ji5489@gmail.com>

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+    express = require('express'),
+    expressErrorHandler = require('express-error-handler'),
+    //static = require('serve-static'),
+    //path = require('path'),
+    
+    mongoose = require('mongoose');
+
+var app = express();
+app.set('port', process.env.PORT || 14402);
+
