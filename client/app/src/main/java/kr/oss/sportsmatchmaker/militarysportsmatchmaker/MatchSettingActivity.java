c76fb403a5f9cef7e48a55bae90f4ebde4be7516
dnfwlxo11 <dnfwlxo11@naver.com>
2017-10-17 20:43:26 +0900

    SearchPlayer(Alpha)

--- client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/MatchSettingActivity.java
+++ client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/MatchSettingActivity.java
@@ -1,10 +1,13 @@
 package kr.oss.sportsmatchmaker.militarysportsmatchmaker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -24,25 +27,24 @@ public class MatchSettingActivity extends AppCompatActivity {
         int playernum = intent.getIntExtra("BIRTHDAY_KEY", 0);
         String[] name = null;
 
-        for(int i =0;i<playernum;i++){
-            ListData data = new ListData("0"+i+".jpg", i+".", i+".");
+        for(int i =1;i<=playernum;i++){
+            ListData data = new ListData("01.jpg", "선수 추가", "");
             listDataArray.add(data);
         }
 
         ListView listview = (ListView) findViewById(R.id.listview1);
         CustomAdapter customAdapter = new CustomAdapter(this, R.layout.list_btn_sty, listDataArray);
         listview.setAdapter(customAdapter);
-
     }
+}
 
-
+/*
     public String[] setnum(String[] player ,int num){
         player = new String[num];
 
         for(int i=0;i<num;i++){
-            player[i] = "선수추가";
+            player[i] = "선수추가"+i;
         }
 
         return player;
-    }
-}
+    }*/
