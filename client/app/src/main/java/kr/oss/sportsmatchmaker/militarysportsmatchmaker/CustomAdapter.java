1102344cd852466e3b7e188caf01e730cca2063f
dnfwlxo11 <dnfwlxo11@naver.com>
2017-10-18 11:14:51 +0900

    선수검색수정중

--- client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/CustomAdapter.java
+++ client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/CustomAdapter.java
@@ -26,7 +26,6 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CustomAdapter extends ArrayAdapter<ListData>{
     private Context context;
@@ -40,6 +39,8 @@ public class CustomAdapter extends ArrayAdapter<ListData>{
         this.listData = listData;
     }
 
+    String Information[][] = {{"00", "01", "02"}, {"소령 이무기", "대장 강정호", "소위 김찬양"}};
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         View row = convertView;
@@ -49,8 +50,9 @@ public class CustomAdapter extends ArrayAdapter<ListData>{
             row = inflater.inflate(layoutResource, parent, false);
         }
 
-        ImageView face = (ImageView) row.findViewById(R.id.Profile);
-        TextView name = (TextView) row.findViewById(R.id.Name);
+        final ImageView face = (ImageView) row.findViewById(R.id.Profile);
+        final TextView name = (TextView) row.findViewById(R.id.Name);
+        final TextView armnum = (TextView) row.findViewById(R.id.Arm_Num);
         Button button = (Button) row.findViewById(R.id.button);
 
         try{
@@ -62,6 +64,7 @@ public class CustomAdapter extends ArrayAdapter<ListData>{
         }
         name.setText(listData.get(position).getName());
         button.setText(listData.get(position).getButton());
+        armnum.setText(listData.get(position).getArm_Num());
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +88,15 @@ public class CustomAdapter extends ArrayAdapter<ListData>{
                                     @Override
                                     public void onClick(
                                             DialogInterface dialog, int id) {
-                                        
+
+                                            if(SearchPlayer(search.getText().toString()) < 0)
+                                                Toast.makeText(context,"해당 선수가 없습니다.",Toast.LENGTH_SHORT).show();
+                                            else {
+                                                armnum.setText(search.getText().toString());
+                                                name.setText(Information[1][SearchPlayer(armnum.getText().toString())]);
+                                                face.setImageResource(R.drawable.img_basketball);
+                                            }
+
                                     }
                         });
 
@@ -95,5 +106,16 @@ public class CustomAdapter extends ArrayAdapter<ListData>{
         });
 
         return row;
+    }
+
+    public int SearchPlayer(String armnum){
+        int num = -1;
+
+        for(int i=0;i<3;i++) {
+            if (Information[0][i] == armnum)
+                num = i;
+        }
+
+        return num;
     }
 }
