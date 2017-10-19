b351aed1dc0a45bdfbef1af9c3c11fb956312fcd
dnfwlxo11 <dnfwlxo11@naver.com>
2017-10-20 05:20:32 +0900

    주석다는중

--- client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/FirstActivity.java
+++ client/app/src/main/java/kr/oss/sportsmatchmaker/militarysportsmatchmaker/FirstActivity.java
@@ -11,10 +11,17 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*
+         상태바 없애는 코드
+         */
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        /*
+         1.8초간 메인 화면을 보여주고 다음 화면으로 넘어가게 하기위한 코드
+         */
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable()  {
             public void run() {
